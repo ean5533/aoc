79c2d01138ec37d79657206f3fea502da292b0a2
Evan Nelson <evan.nelson@elastic.co>
2021-12-01 07:49:27 -0800

    Day part 2

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,7 +1,21 @@
 package day1
 
 fun main() {
+    part1()
+    part2()
+}
+
+fun part1() {
     val increases = input
+        .zipWithNext { a, b -> if (b > a) 1 else 0 }
+        .sum()
+    println(increases)
+}
+
+fun part2() {
+    val increases = input
+        .windowed(3, 1)
+        .map { it.sum() }
         .zipWithNext { a, b -> if (b > a) 1 else 0 }
         .sum()
     println(increases)
