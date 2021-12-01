d1555eda9e5c5e480e9f19282dc5a42ab812f085
Evan Nelson <evan.nelson@elastic.co>
2021-12-01 08:10:33 -0800

    Use proper organization, resource file

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,19 +1,23 @@
 package day1
 
+private val classLoader: ClassLoader = object {}.javaClass.classLoader
+private val input = classLoader.getResource("text/day1")!!.readText()
+private val numbers: List<Int> = input.lines().map { it.trim().toInt() }
+
 fun main() {
     part1()
     part2()
 }
 
 fun part1() {
-    val increases = input
+    val increases = numbers
         .zipWithNext { a, b -> if (b > a) 1 else 0 }
         .sum()
     println(increases)
 }
 
 fun part2() {
-    val increases = input
+    val increases = numbers
         .windowed(3, 1)
         .map { it.sum() }
         .zipWithNext { a, b -> if (b > a) 1 else 0 }
