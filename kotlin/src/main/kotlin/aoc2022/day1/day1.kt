43b67cbb6ee4189a442179563875f954add78475
Evan Nelson <enelson@squareup.com>
2022-12-01 07:44:27 -0800

    Day1

--- kotlin/src/main/kotlin/aoc2022/day1/day1.kt
+++ kotlin/src/main/kotlin/aoc2022/day1/day1.kt
@@ -3,6 +3,8 @@ package aoc2022.day1
 import lib.loadResourceAsString
 
 private val input = loadResourceAsString("text/aoc2022/day1")
+private val calorieLists = input.split("\n\n").map { it.split("\n").mapNotNull { it.toIntOrNull() } }
+private val sortedSums = calorieLists.map { it.sum() }.toList().sortedDescending()
 
 fun main() {
     part1()
@@ -10,7 +12,9 @@ fun main() {
 }
 
 private fun part1() {
+    println("max = ${sortedSums.first()}")
 }
 
 private fun part2() {
+    println("max3 = ${sortedSums.take(3).sum()}")
 }
---
+++ kotlin/src/main/kotlin/aoc2022/day2/day2.kt
@@ -0,0 +1,16 @@
+package aoc2022.day2
+
+import lib.loadResourceAsString
+
+private val input = loadResourceAsString("text/aoc2022/day2")
+
+fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
+}
+
+private fun part2() {
+}
