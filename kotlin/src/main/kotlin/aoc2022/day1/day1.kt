3552eb623cb466e678352d4c7db696a935a0e2e9
Evan Nelson <enelson@squareup.com>
2022-12-04 22:25:25 -0800

    Day 5

--- kotlin/src/main/kotlin/aoc2022/day1/day1.kt
+++ kotlin/src/main/kotlin/aoc2022/day1/day1.kt
@@ -3,7 +3,7 @@ package aoc2022.day1
 import lib.loadResourceAsString
 import lib.split
 
-private val input = loadResourceAsString("text/aoc2022/day1")
+private val input = loadResourceAsString("text/aoc2022/day1").trim()
 private val calorieLists = input.lines().split("").map { it.map { it.toInt() } }
 private val sortedSums = calorieLists.map { it.sum() }.toList().sortedDescending()
 
---
+++ kotlin/src/main/kotlin/aoc2022/day5/day5.kt
@@ -0,0 +1,70 @@
+package aoc2022.day5
+
+import lib.loadResourceAsString
+
+private val input = loadResourceAsString("text/aoc2022/day5").lines()
+
+fun main() {
+    val (warehouse, moves) = parseInput()
+    part1(warehouse, moves)
+    part2(warehouse, moves)
+}
+
+private fun part1(warehouse: Warehouse, moves: List<Move>) {
+    val moved = warehouse.move(moves, CrateMover9000)
+    println("tops1 = ${moved.stacks.map { it.first() }.joinToString("")}")
+}
+
+private fun part2(warehouse: Warehouse, moves: List<Move>) {
+    val moved = warehouse.move(moves, CrateMover9001)
+    println("tops2 = ${moved.stacks.map { it.first() }.joinToString("")}")
+}
+
+private fun parseInput(): Pair<Warehouse, List<Move>> {
+    val stackCount = input.single { it.startsWith(" 1") }.split("   ").count()
+    val stackLines = input.takeWhile { it.contains('[') }
+    val stacks = (0 until stackCount).map { stackNum ->
+        (0 until stackLines.count()).map { input[it][1 + stackNum * 4] }.filterNot { it == ' ' }
+    }
+    val warehouse = Warehouse(stacks)
+
+    val moves = input.drop(stackLines.count() + 2).takeWhile { it.startsWith("move") }.map {
+        val pieces = it.split(' ')
+        Move(pieces[1].toInt(), pieces[3].toInt(), pieces[5].toInt())
+    }
+    
+    return warehouse to moves
+}
+
+data class Warehouse(val stacks: List<List<Char>>) {
+    fun move(moves: Iterable<Move>, crane: Crane) : Warehouse =
+        moves.fold(this) { warehouse, move -> warehouse.move(move, crane) }
+    
+    fun move(move: Move, crane: Crane): Warehouse {
+        val (newFrom, newTo) = crane.move(stacks[move.from - 1], stacks[move.to - 1], move.amount)
+        val newStacks = stacks.mapIndexed { index, stack ->
+            when (index) {
+                move.from - 1 -> newFrom
+                move.to - 1 -> newTo
+                else -> stack
+            }
+        }
+        return Warehouse(newStacks)
+    }
+}
+
+data class Move(val amount: Int, val from: Int, val to: Int)
+
+interface Crane {
+    fun move(from: List<Char>, to: List<Char>, amount: Int): Pair<List<Char>, List<Char>>
+}
+
+object CrateMover9000 : Crane {
+    override fun move(from: List<Char>, to: List<Char>, amount: Int): Pair<List<Char>, List<Char>> =
+        from.drop(amount) to (from.take(amount).reversed() + to)
+}
+
+object CrateMover9001 : Crane {
+    override fun move(from: List<Char>, to: List<Char>, amount: Int): Pair<List<Char>, List<Char>> =
+        from.drop(amount) to (from.take(amount) + to)
+}
