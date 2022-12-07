bc2b2a346023f5e8619fdd6eb5ba4eb249a5f5ef
Evan Nelson <enelson@squareup.com>
2022-12-06 22:40:22 -0800

    Day 7

---
+++ kotlin/src/main/kotlin/aoc2022/day7/day7minimal.kt
@@ -0,0 +1,56 @@
+package aoc2022.day7
+
+import lib.loadResourceAsString
+
+private val input = loadResourceAsString("text/aoc2022/day7").trim()
+private val allDirs = parseInput()
+
+/**
+ * A simple, hacky solution that makes a lot of assumptions but sufficiently solves both parts.
+ * 
+ * See day7tree.kt for a more flexible, exensible solution that builds a tree structure.
+ */
+fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
+    val smallDirs = allDirs.filter { it.size <= 100000 }
+    println("part1 = ${smallDirs.sumOf { it.size }}")
+}
+
+private fun part2() {
+    val spaceNeeded = 30000000 - (70000000 - allDirs.first().size)
+    val smallestDeletionToSatify = allDirs
+        .map { it.size }
+        .filter { it > spaceNeeded }
+        .minByOrNull { it }!!
+    println("part2 = $smallestDeletionToSatify")
+}
+
+private fun parseInput(): List<Dir> {
+    val allDirs = mutableListOf<Dir>()
+    val stack = mutableListOf<Dir>()
+    input.lines().forEach {
+        when {
+            it == "$ cd .." ->
+                stack.removeLast()
+
+            it.startsWith("$ cd") -> {
+                val dir = Dir()
+                stack.add(dir)
+                allDirs.add(dir)
+            }
+
+            it[0].digitToIntOrNull() != null -> {
+                val size = it.split(" ")[0].toLong()
+                stack.forEach { it.size += size }
+            }
+        }
+    }
+    
+    return allDirs
+}
+
+class Dir(var size: Long = 0)
---
+++ kotlin/src/main/kotlin/aoc2022/day7/day7tree.kt
@@ -0,0 +1,76 @@
+package aoc2022.day7
+
+import lib.loadResourceAsString
+
+private val input = loadResourceAsString("text/aoc2022/day7").trim()
+private val root = Directory("/")
+
+/**
+ * My original solution before finding out part 2 was trivial and didn't require any of the tree structure stuff.
+ * 
+ * See day7minimal.kt for a simpler, hackier solution.
+ */
+fun main() {
+    parseInput()
+    part1()
+    part2()
+}
+
+private fun parseInput() {
+    val dirMap = mutableMapOf(root.path to root)
+    var currentDirectory = root
+    input.lines().drop(1).forEach {
+        when {
+            it == "$ cd .." ->
+                currentDirectory = dirMap[currentDirectory.parentPath]!!
+            it.startsWith("$ cd") -> {
+                val target = it.split(" ")[2]
+                currentDirectory = dirMap[currentDirectory.path + target + "/"]!!
+            }
+            it == "$ ls" -> {}
+            it.startsWith("dir") -> {
+                val dir = Directory(currentDirectory.path + it.split(" ")[1] + "/")
+                currentDirectory.contents.add(dir)
+                dirMap.put(dir.path, dir)
+            }
+            it[0].digitToIntOrNull() != null -> {
+                val (size, name) = it.split(" ")
+                currentDirectory.contents.add(File(currentDirectory.path + name, size.toLong()))
+            }
+            else -> throw NotImplementedError()
+        }
+    }
+}
+
+private fun part1() {
+    val smallDirsSum = root.getRecursive { it is Directory && it.size <= 100000 }.sumOf { it.size }
+    println("part1 = $smallDirsSum")
+}
+
+private fun part2() {
+    val spaceNeeded = 30000000 - (70000000 - root.size)
+    val smallestDeletionToSatify = root.getRecursive { it is Directory }
+        .map { it.size }
+        .filter { it > spaceNeeded }
+        .minByOrNull { it }!!
+    println("part2 = $smallestDeletionToSatify")
+}
+
+private interface Element {
+    val size: Long
+}
+
+private data class Directory(val path: String, val contents: MutableList<Element> = mutableListOf()) : Element {
+    fun getRecursive(predicate: (Element) -> Boolean): List<Element> {
+        return contents.filter(predicate) + contents.filterIsInstance<Directory>()
+            .flatMap { it.getRecursive(predicate) }
+    }
+
+    val parentPath: String? =
+        if (path == "/") null else path.trimEnd('/').let { it.substring(0, it.lastIndexOf("/") + 1) }
+
+    override val size: Long
+        get() = contents.sumOf { it.size }
+}
+
+private data class File(val path: String, override val size: Long) : Element
