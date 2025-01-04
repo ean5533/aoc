be7a874fafb0698e5b0af2afc839bd4bb26d573b
Evan Nelson <enelson@squareup.com>
2025-01-03 21:05:32 -0800

    Day 20 part 2

--- kotlin/src/main/kotlin/aoc2024/day20/day20.kt
+++ kotlin/src/main/kotlin/aoc2024/day20/day20.kt
@@ -6,35 +6,60 @@ private val input = loadResourceMatchingPackageName(object {}.javaClass).trim()
 private val track = parseInput()
 
 fun main() {
+  val path = generateSequence(track.car to track.car) { (previous, current) ->
+    val dest = current.neighbors4().filter { it != previous && !track.walls.contains(it) }
+    if (dest.isEmpty()) null else current to dest.single()
+  }.takeWhile { it.first != track.destination }.toList()
+  val timeWithoutCheat = path.size - 1
+  println("Without cheating: $timeWithoutCheat")
+  
+  part1(path)
+  part2(path)
+}
+
+private fun part1(pathTimes: List<Pair<Point2D, Point2D>>) {
   printTimeTaken {
-    val path = generateSequence(track.car to track.car) { (previous, current) ->
-      val dest = current.neighbors4().filter { it != previous && !track.walls.contains(it) }
-      if(dest.isEmpty()) null else current to dest.single()
-    }.takeWhile { it.first != track.destination }.toList()
-    val timeWithoutCheat = path.size - 1
-    println("Without cheating: $timeWithoutCheat")
-    
-    val timeMap = path.reversed().mapIndexed { index, (_, position) -> position to index }.toMap()
-//    println(timeMap)
-    
+    val timeWithoutCheat = pathTimes.size - 1
+    val timeMap = pathTimes.reversed().mapIndexed { index, (_, position) -> position to index }.toMap()
+
+    // For every wall, find out how far you could get if you phased through it
     val cheats = track.walls.mapNotNull { wall ->
       val neighbors = wall.neighbors4()
         .filter { track.area.contains(it) && !track.walls.contains(it) }
         .sortedByDescending { timeMap[it] }
-      if(neighbors.size < 2) {
+      if (neighbors.size < 2) {
         return@mapNotNull null
       }
-      
+
       val source = neighbors.first()
       val destination = neighbors.last()
-      
+
       val timeWithCheat = (timeWithoutCheat - timeMap[source]!!) + timeMap[destination]!! + 2
       timeWithCheat
     }
-    
-//    println(cheats)
-//    println(cheats.map { timeWithoutCheat - it }.groupingBy { it }.eachCount().toSortedMap())
+
+    // Count the ones where the time saved was at least 100
     println(cheats.map { timeWithoutCheat - it }.count { it >= 100 })
+  }
+}
+
+private fun part2(path: List<Pair<Point2D, Point2D>>) {
+  printTimeTaken {
+    val timeWithoutCheat = path.size - 1
+    val goalTime = timeWithoutCheat - 100
+    val timeMap = path.reversed().mapIndexed { index, (_, position) -> position to index }.toMap()
+    
+    // For every point on the path, count all other points on the path <=20 spaces away and find out how much time would be saved by phasing to them.
+    // Don't bother checking any path points that are already within 100 of the goal
+    // This solution would also work for part 1, but it's slower.
+    val fasterRoutes = timeMap.entries.filter { it.value > 100 }.sumOf { (source, timeFromSource) ->
+      val timeToSource = timeWithoutCheat - timeFromSource
+      timeMap.keys
+        .filter { source.manhattanDistanceTo(it) <= 20 }
+        .map { timeToSource + source.manhattanDistanceTo(it) + timeMap[it]!! }
+        .count { it <= goalTime }
+    }
+    println(fasterRoutes)
   }
 }
 
